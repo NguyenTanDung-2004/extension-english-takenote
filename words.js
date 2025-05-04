661734a24e3bcab471d8cf55d1c73ccbd01c8f65
NguyenTanDung-2004 <22520001@gm.uit.edu.vn>
2025-05-04 22:13:15 +0700

    first commit

---
+++ words.js
@@ -0,0 +1,23 @@
+const wordList = [
+    {
+      word: "procrastinate",
+      meaning: "trì hoãn",
+      example: "Don’t procrastinate your work until the last minute!"
+    },
+    {
+      word: "meticulous",
+      meaning: "tỉ mỉ, kỹ lưỡng",
+      example: "She is very meticulous about her work."
+    },
+    {
+      word: "versatile",
+      meaning: "đa năng",
+      example: "He is a versatile developer who can work on both frontend and backend."
+    },
+    {
+      word: "concise",
+      meaning: "ngắn gọn, súc tích",
+      example: "Keep your email concise and clear."
+    }
+  ];
+  
